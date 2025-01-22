95ac570ec028fdd00717213a1cb0dc6fb5d1d944
bijaya-dulal <dulalbijaya0@gmail.com>
2025-01-22 23:25:03 +0545

    server overview designed

--- assignment1/server.c
+++ assignment1/server.c
@@ -4,3 +4,24 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+
+#define PORT 8081
+#define BUFFER_SIZE 1024
+
+ int main(){
+   int server_fd, new_socket1 , new_socket2;
+   struct sockaddr_in address;
+   int addrlen = sizeof(address);
+    //create socket
+
+    //bind socket to port
+
+    //listen for incoming connections
+
+    //accept clinet
+
+    //game logic
+    
+    //close sockets
+
+ }
